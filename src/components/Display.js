09b6cabfae7414cf708923137df0c0fd12068738
NewIncome <alfredo_cardenas@live.com>
2020-11-30 15:07:18 -0600

    Add and implement the display component with: - accepting an incoming prop for the result of teh calculation - restrincting the type of the prop to a string - defaulting the result prop to 0

---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export default function Display(props) {
+  const { result } = props;
+
+  return (
+    <div className="display">
+      {result}
+    </div>
+  );
+}
+
+Display.defaultProps = { result: '0' };
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
