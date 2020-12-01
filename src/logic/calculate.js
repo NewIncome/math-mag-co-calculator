98c54b117ea10a3f7ceb15c24d8b5119b6752b31
NewIncome <alfredo_cardenas@live.com>
2020-12-01 16:10:55 -0600

    Implement a basic version of the calculate.js module
    - add the buttonName '+/-' implementation

---
+++ src/logic/calculate.js
@@ -0,0 +1,12 @@
+export default function calculate(calculatorData, buttonName) {
+  let total;
+  let next;
+  const operation = '';
+
+  if (buttonName === '+/-') {
+    total *= -1;
+    next *= -1;
+  }
+
+  return calculatorData;
+}
