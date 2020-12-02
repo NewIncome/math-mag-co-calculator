5f44ad1d5859539ab0692e4466c8f500d354ae46
NewIncome <alfredo_cardenas@live.com>
2020-12-02 14:52:56 -0600

    Make App a stateful component to be the parent elem with control
    - give it a state and inside the total, next and operation vars
    - implement the handleClick func for the buttonPanel
    - pass total or next values to Display

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
+import React from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display result={0} />
-      <ButtonPanel />
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    let { newTotal, newNext, newOperation } = Calculate(...this.state);
+    this.setState({
+      total: newTotal,
+      next: newNext,
+      operation: newOperation,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Display result={this.total || this.next} />
+        <ButtonPanel OnClick={this.handleClick} />
+      </>
+    );
+  }
 }
 
 export default App;
