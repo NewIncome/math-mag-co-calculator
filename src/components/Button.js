4dcc0b4413decb7f569e24aeee9bd77881255cc4
NewIncome <alfredo_cardenas@live.com>
2020-12-02 15:30:03 -0600

    Implement the handleClick func for the button func element

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,19 @@ import PropTypes from 'prop-types';
 
 export default function Button(props) {
   const { name } = props;
+
+  function handleClick(buttonName) {
+    props.onClick(buttonName);
+  }
+
   return (
-    <button type="button" id={name}>{ name }</button>
+    <button
+      type="button"
+      id={name}
+      onClick={handleClick}
+    >
+      { name }
+    </button>
   );
 }
 
