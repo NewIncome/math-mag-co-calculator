c6f453d1bc4f08c82310f6614d6135ca586dfc6b
NewIncome <alfredo_cardenas@live.com>
2020-12-03 21:40:55 -0600

    Fix some operation errors in calculate
    - it let's you click on sign buttons multiple times without error

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -3,50 +3,51 @@ import operate from './operate';
 export default function calculate(calculatorData, buttonName) {
   let { total, next, operation } = calculatorData;
 
-  console.log('next at start:');
   if (Number(buttonName)) {
-    if (next === '0') next = buttonName;
+    if (next === '0' || (operation && next === null) || !next) next = buttonName;
     else next += buttonName;
   }
-  console.log(next);
-  console.log('buttonName:');
-  console.log(buttonName);
-  console.log(typeof buttonName);
-  console.log(buttonName === '-' ? 'MINUS!!!' : 'Fucked');
-  switch (buttonName) {
-    case '+/-' && buttonName !== 0:
-      // total *= -1;
-      next *= -1;
-      break;
-    case '%':
-      next /= 100;
-      break;
-    case '.':
-      if (next % 1 === 0) next += '.';
-      break;
-    case 'A/C':
-      total = null;
-      next = '0';
-      break;
-    case '+':
-    case '-':
-    case 'X':
-    case '/':
-      console.log('GOT INTO THE OPERATIONS!!!');
-      total = next;
-      next = '0';
-      operation = buttonName;
-      break;
-    case '=':
-      total = operate(total, next, operation);
-      next = null;
-      break;
-    default:
-      break;
-  }
 
-  console.log('next:');
-  console.log(next);
+  if (buttonName !== operation) {
+    if (operation && ['+', '-', 'X', '/'].includes(buttonName)) {
+      next = total;
+      total = null;
+      operation = null;
+    }
+
+    switch (buttonName) {
+      case '+/-' && buttonName !== 0:
+        // total *= -1;
+        next *= -1;
+        break;
+      case '%':
+        next /= 100;
+        break;
+      case '.':
+        if (next % 1 === 0) next += '.';
+        break;
+      case 'A/C':
+        total = null;
+        next = '0';
+        break;
+      case '+':
+      case '-':
+      case 'X':
+      case '/':
+        total = next;
+        next = null;
+        operation = buttonName;
+        break;
+      case '=':
+        if (Number(next) && Number(total)) {
+          total = operate(total, next, operation);
+          next = null;
+        }
+        break;
+      default:
+        break;
+    }
+  }
 
   return { total, next, operation };
 }
