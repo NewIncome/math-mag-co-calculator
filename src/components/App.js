0de501b4e928b7b5f65ec72dccae3086db60d4d1
NewIncome <alfredo_cardenas@live.com>
2020-12-04 11:07:03 -0600

    Fix '0' not working right and initialize next to null

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
 
     this.state = {
       total: null,
-      next: '0',
+      next: null,
       operation: null,
     };
     this.handleClick = this.handleClick.bind(this);
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -8,46 +8,45 @@ export default function calculate(calculatorData, buttonName) {
     operation = null;
   }
 
-  if (Number(buttonName)) {
-    if (next === '0' || (operation && next === null) || !next) next = buttonName;
+  if (Number(buttonName) || buttonName === '0') {
+    if (next === null) next = buttonName;
     else next += buttonName;
   }
 
-  if (buttonName !== operation) {
-    switch (buttonName) {
-      case '+/-' && buttonName !== 0:
-        // total *= -1;
-        next *= -1;
-        break;
-      case '%':
-        next /= 100;
-        break;
-      case '.':
-        if (next % 1 === 0) next += '.';
-        break;
-      case 'A/C':
-        total = null;
-        next = null;
-        operation = null;
-        break;
-      case '+':
-      case '-':
-      case 'X':
-      case '/':
-        total = next;
+  switch (buttonName) {
+    case '+/-':
+      // total *= -1;
+      next *= -1;
+      next = next.toString();
+      break;
+    case '%':
+      next /= 100;
+      break;
+    case '.':
+      if (next % 1 === 0) next += '.';
+      break;
+    case 'A/C':
+      total = null;
+      next = null;
+      operation = null;
+      break;
+    case '+':
+    case '-':
+    case 'X':
+    case '/':
+      total = next;
+      next = null;
+      operation = buttonName;
+      break;
+    case '=':
+      if (Number(next) && Number(total)) {
+        total = operate(total, next, operation);
         next = null;
         operation = buttonName;
-        break;
-      case '=':
-        if (Number(next) && Number(total)) {
-          total = operate(total, next, operation);
-          next = null;
-          operation = buttonName;
-        }
-        break;
-      default:
-        break;
-    }
+      }
+      break;
+    default:
+      break;
   }
 
   return { total, next, operation };
