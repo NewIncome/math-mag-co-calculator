e8b2933a9878110069a38d6028ed12c421223c7e
NewIncome <alfredo_cardenas@live.com>
2021-01-12 13:35:07 -0600

    Refactor the App class to be a functional component & use statehooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [calcVars, setCalcVars] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(buttonName) {
-    let data = this.state;
+  const handleClick = buttonName => {
+    let data = calcVars;
     data = Calculate(data, buttonName);
-    const { total, next, operation } = data;
 
-    this.setState(state => ({
+    setCalcVars({
+      ...calcVars,
       total: data.total,
       next: data.next,
       operation: data.operation,
-    }));
-  }
+    });
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
+  // const { total, next } = calcVars;
 
-    return (
-      <>
-        <Display result={
-          (total === null && next === null) ? '0' : next === null ? total : next
-          }
-        />
-        <ButtonPanel onClick={this.handleClick} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Display result={
+        (calcVars.total === null && calcVars.next === null) ? '0' : calcVars.next === null ? calcVars.total : calcVars.next
+        }
+      />
+      <ButtonPanel onClick={handleClick} />
+    </>
+  );
+};
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick(buttonName) {
+//     let data = this.state;
+//     data = Calculate(data, buttonName);
+//     const { total, next, operation } = data;
+
+//     this.setState(state => ({
+//       total: data.total,
+//       next: data.next,
+//       operation: data.operation,
+//     }));
+//   }
+
+//   render() {
+//     const { total, next, operation } = this.state;
+
+//     return (
+//       <>
+//         <Display result={
+//           (total === null && next === null) ? '0' : next === null ? total : next
+//           }
+//         />
+//         <ButtonPanel onClick={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
 
 export default App;
