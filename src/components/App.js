db10a6adceafed01c18a351833d337b8237a60e3
NewIncome <alfredo_cardenas@live.com>
2021-01-13 20:40:24 -0600

    Fix styles for calculator section and modify section tag
    - remov section tag from routes and add it to each page

--- src/components/App.js
+++ src/components/App.js
@@ -31,19 +31,22 @@ class App extends React.Component {
     const { total, next } = this.state;
 
     return (
-      <div id="calcContainer">
-        <div id="cBody">
-          <div className="calcTop">
-            <Display result={
-              (total === null && next === null) ? '0' : next === null ? total : next
-              }
-            />
-          </div>
-          <div className="calcBtm">
-            <ButtonPanel onClick={this.handleClick} />
+      <section className="section1">
+        <p>Let&apos;s do some math!</p>
+        <div id="calcContainer">
+          <div id="cBody">
+            <div className="calcTop">
+              <Display result={
+                (total === null && next === null) ? '0' : next === null ? total : next
+                }
+              />
+            </div>
+            <div className="calcBtm">
+              <ButtonPanel onClick={this.handleClick} />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
--- src/components/DayQuote.js
+++ src/components/DayQuote.js
@@ -1,10 +1,10 @@
 const DayQuote = () => (
-  <>
+  <section className="section">
     <h2 className="title">Quote of the Day</h2>
     <blockquote>
       Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston
     </blockquote>
-  </>
+  </section>
 );
 
 export default DayQuote;
