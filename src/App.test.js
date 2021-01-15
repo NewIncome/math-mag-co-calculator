752dfc3ab4ef92b83a3157d9df9aae43bcd94dca
NewIncome <alfredo_cardenas@live.com>
2021-01-14 22:44:09 -0600

    Finished unit tests on calculate.js, react tests not working yet
    - Tried installing babel and other components and other config
    files and no luck yet for React component tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import Home from './components/Home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Welcome to our page!', () => {
+  render(<Home />);
+  const titleElement = screen.getByText(/Welcome to our page!/i);
+  expect(titleElement).toBeInTheDocument();
 });
