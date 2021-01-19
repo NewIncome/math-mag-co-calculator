64730c450bfec2e506614c9f848613569c93cd81
NewIncome <alfredo_cardenas@live.com>
2021-01-19 14:29:00 -0600

    Fix linter errors, Fix Jest error, Add a last test for calcPage
    - Add the React import (also) to Button.js
    - Remove unused App.test.js
    - Add filter for stylelint to not check a 'prettier' directory

--- src/components/Timeline/__tests__/CalculatorPage.test.js
+++ src/components/Timeline/__tests__/CalculatorPage.test.js
@@ -3,7 +3,32 @@ import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
 import CalculatorPage from '../../CalculatorPage';
 
-it('renders correctly', () => {
+it('calculator page renders correctly', () => {
   const tree = renderer.create(<CalculatorPage />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('calculator page renders exactly all HTML elements', () => {
+  const tree = renderer
+    .create(<CalculatorPage />)
+    .toJSON();
+  expect(tree).toMatchSnapshot(`
+  <section class="section1">
+    <p>Let's do some math!</p>
+    <div id="calcContainer">
+      <div id="cBody">
+        <div class="calcTop">
+          <div class="display"></div>
+        </div>
+        <div class="calcBtm">
+          <div class="group1 group"></div>
+          <div class="group2 group"></div>
+          <div class="group3 group"></div>
+          <div class="group4 group"></div>
+          <div class="group5 group"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  `);
 });
